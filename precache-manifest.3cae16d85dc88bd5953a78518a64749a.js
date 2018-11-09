7e97256256712247fce3bd783fb82fd443026dda
cinfusino <cinfusino@csu.fullerton.edu>
2018-11-09 15:36:54 -0800

    Updates

---
+++ precache-manifest.3cae16d85dc88bd5953a78518a64749a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bd89aec6ea1680cf588",
+    "url": "/cpsc-473-assignment-2/static/css/main.beed64b6.chunk.css"
+  },
+  {
+    "revision": "1bd89aec6ea1680cf588",
+    "url": "/cpsc-473-assignment-2/static/js/main.1bd89aec.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/cpsc-473-assignment-2/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "091ffaaa9c6ba60d6958",
+    "url": "/cpsc-473-assignment-2/static/js/runtime~main.091ffaaa.js"
+  },
+  {
+    "revision": "e2846d579918f1a7ff0bc642e5169c96",
+    "url": "/cpsc-473-assignment-2/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cpsc-473-assignment-2/precache-manifest.3cae16d85dc88bd5953a78518a64749a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cpsc-473-assignment-2/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
